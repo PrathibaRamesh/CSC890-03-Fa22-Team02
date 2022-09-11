65202218df19ae99d67eadba67fd4303ba1a7e09
PrathibaRamesh <prathibaeie1@gmail.com>
2022-09-11 16:13:58 -0700

    import React issue resolved and Bootstrap package added

--- sfsu-csc-help/frontend/src/components/homepage/about.component.js
+++ sfsu-csc-help/frontend/src/components/homepage/about.component.js
@@ -1,52 +1,12 @@
 import React, { Component } from "react";
+import Navbar from "./NavBar";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <header id="header" class="header fixed-top d-flex align-items-center">
-          <div class="container d-flex align-items-center justify-content-between">
-            <a href="/" class="logo d-flex align-items-center me-auto me-lg-0">
-              <img src="assets/img/logo.png" alt="" />
-            <h1><span>|</span> Department of Computer Science</h1>
-            </a>
-
-            <nav id="navbar" class="navbar">
-            <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#events">Events</a></li>
-              <li class="dropdown"><a href="#"><span>Menu</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                <ul>
-                <li class="dropdown"><a href="#"><span>Undergraduate</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                    <ul>
-                      <li><a href="#">Deep Drop Down 1</a></li>
-                      <li><a href="#">Deep Drop Down 2</a></li>
-                      <li><a href="#">Deep Drop Down 3</a></li>
-                      <li><a href="#">Deep Drop Down 4</a></li>
-                      <li><a href="#">Deep Drop Down 5</a></li>
-                    </ul>
-                  </li>
-                  <li class="dropdown"><a href="#"><span>Graduate</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                    <ul>
-                      <li><a href="#">Deep Drop Down 1</a></li>
-                      <li><a href="#">Deep Drop Down 2</a></li>
-                      <li><a href="#">Deep Drop Down 3</a></li>
-                      <li><a href="#">Deep Drop Down 4</a></li>
-                      <li><a href="#">Deep Drop Down 5</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-            </nav>
-
-            <a class="btn-book-a-table" href="#sign-up">Sign Up</a>
-            <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-            <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-          </div>
-        </header>
+        <Navbar />
+        
 
         <section id="home" class="hero d-flex align-items-center section-bg">
           <div class="container">
