f1b5d0a7b5a061dc00d571d8e4a63c5b3f1375f0
PrathibaRamesh <prathibaeie1@gmail.com>
2022-09-11 18:48:09 -0700

    Added the team schedule and plan in about page

--- sfsu-csc-help/frontend/src/components/homepage/about.component.js
+++ sfsu-csc-help/frontend/src/components/homepage/about.component.js
@@ -67,6 +67,45 @@ export default class About extends Component {
               </div>
           </div>
         </section>
+
+      <section id="menu" class="menu">
+      <div class="container" data-aos="fade-up">
+      <div class="section-header">
+          <p>Team Plan and Schedule </p>
+        </div>
+        <div class="tab-content" data-aos="fade-up" data-aos-delay="300">
+          <div class="tab-pane fade active show" id="menu-starters">
+            <div class="row gy-5">
+              <div class="col-lg-6 menu-item">
+                <h4>Communication</h4>
+                <p class="ingredients">
+                  Discord Channel
+                </p>  
+                <br />           
+                <h4>Weekly meeting</h4>
+                <p class="ingredients">
+                  Friday @ 3PM PST via zoom
+                </p>
+              </div>
+
+              <div class="col-lg-6 menu-item">
+                <h4>Learning Plan</h4>
+                <p class="ingredients">
+                  September - OpenAI Learning
+                </p>
+                <p class="ingredients">
+                  October - Integrating Open AI into tech stack
+                </p>
+                <p class="ingredients">
+                  November - Fine Tunning in Open AI
+                </p>
+              </div>
+
+            </div>
+          </div>
+          </div>
+      </div>
+      </section>
         </main>
         
       </div>
