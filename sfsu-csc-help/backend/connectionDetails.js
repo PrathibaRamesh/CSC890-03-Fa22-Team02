d19968debb6aae8afde10540dd8d274a2332148f
anudeepkatukojwala <anudeep.katukojwala@gmail.com>
2022-11-06 22:19:48 -0800

    user input validations and password hashing implemented

---
+++ sfsu-csc-help/backend/connectionDetails.js
@@ -0,0 +1,6 @@
+const username = "csc890";
+const hostname = "20.228.204.41";
+const password = "Sfsuhelp890";
+const database = "cschelp";
+
+module.exports = {username, hostname, password, database};
